4c6ec2ecb6eb788828c493fa80fd38b26e7e743e
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-06-16 11:32:55 +0530

    Add files via upload

---
+++ src/com/brainmentors/cms/test/staff/EmployeeTest.java
@@ -0,0 +1,25 @@
+package com.brainmentors.cms.test.staff;
+
+import com.brainmentors.cms.staff.Employee;
+
+public class EmployeeTest {
+
+	public static void main(String[] args) {
+		Employee ram ;
+		ram = new Employee(1001, "Ram");
+		ram.setName("Ramesh");
+		System.out.println(ram.getName());
+		System.out.println(ram.toString());
+		//ram= new Employee();
+//		ram.display();
+		//ram.id =-1001;
+		//ram.salary = -99999;
+		//ram.input(1001, "Ram");
+		//ram.display();
+		Employee shyam = new Employee(1002, "Shyam") ;
+		System.out.println(shyam);
+//		shyam.display();
+
+	}
+
+}
