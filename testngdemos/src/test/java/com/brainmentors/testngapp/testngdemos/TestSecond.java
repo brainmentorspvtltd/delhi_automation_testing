19aa52767e28117272be0bd8d37060496199455f
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2020-08-10 13:21:55 +0530

    Add files via upload

--- testngdemos/src/test/java/com/brainmentors/testngapp/testngdemos/TestSecond.java
+++ testngdemos/src/test/java/com/brainmentors/testngapp/testngdemos/TestSecond.java
@@ -4,7 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestSecond {
-	@Test
+	@Test(threadPoolSize = 3, invocationCount = 12,timeOut = 9000)
+	//@Test(timeOut = 4000)
 	public void test3() {
 		System.out.println("Test3 Call...........");
 		Assert.assertEquals(10, 10);
